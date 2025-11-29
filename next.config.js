646fb9bdb66c8f588877a1aa9dd6a5e14f166441
shrestha-tripathi <33129143+shrestha-tripathi@users.noreply.github.com>
2025-11-29 20:24:43 +0530

    Modify basePath and assetPrefix in next.config.js
    Updated basePath and assetPrefix to empty strings.

--- next.config.js
+++ next.config.js
@@ -2,11 +2,14 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: '/worksoffline',
-  assetPrefix: '/worksoffline/',
+  // basePath: '/worksoffline',
+  // assetPrefix: '/worksoffline/',
+  basePath: '',
+  assetPrefix: '',
   images: {
     unoptimized: true,
   },
 }
 
 module.exports = nextConfig
+
